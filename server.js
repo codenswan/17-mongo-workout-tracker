9672a24356bac50b6a4e232c715eb711085b0faf
Dan Swan <rodger.swan@gmail.com>
2020-09-11 16:40:54 +1000

    morgan logger; require routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+const mongoose = require("mongoose");
 const express = require('express');
 const compression = require('compression');
-const mongoose = require("mongoose");
-const router = require("./routes/allRoutes.js")
+const logger = require("morgan")
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -9,20 +9,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
-
+app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
-
 //* this exposes the whole public folder so that html routes are not necessary; eg app.get("/") not needed
 app.use(express.static("public"));
 
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
-
-app.use(router);
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
