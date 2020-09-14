36265fc16b743bcde10608f38792d1f29804a98c
Dan Swan <rodger.swan@gmail.com>
2020-09-14 14:42:49 +1000

    change selection of last workout to back end

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -2,23 +2,23 @@ const API = {
   async getLastWorkout() {
     let res;
     try {
-      res = await fetch("/api/workouts");
+      res = await fetch("/api/workouts/last");
     } catch (err) {
       console.log(err)
     }
     const json = await res.json();
-
-    return json[json.length - 1];
+    return json
+    // [json.length - 1];
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
-
+    
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
-
+  
     const json = await res.json();
 
     return json;
@@ -29,14 +29,13 @@ const API = {
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
     });
-    console.log(body);
     const json = await res.json();
 
     return json;
   },
 
   async getWorkoutsInRange() {
-    const res = await fetch(`/api/workouts/range`);
+    const res = await fetch("/api/workouts/range");
     const json = await res.json();
 
     return json;
