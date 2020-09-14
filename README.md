ff71acf40c4f2aac8a4c97789b66dcbda26e01eb
Dan Swan <rodger.swan@gmail.com>
2020-09-14 14:47:38 +1000

    all html routes working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 17-mongo-workout-tracker
+
+
+## https://shrouded-peak-65947.herokuapp.com/
+
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,6 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/exercise", (req, res) => {
-  console.log(req.query.body);
   res.sendFile(path.join(__dirname + "./../public/exercise.html"));
 });
 
