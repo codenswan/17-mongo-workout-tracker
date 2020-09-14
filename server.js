72e7f016f06099fb257a22b726e6808bd6e96584
Dan Swan <rodger.swan@gmail.com>
2020-09-14 14:42:02 +1000

    api shortcut for urls

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const mongoose = require("mongoose");
-const express = require('express');
-const compression = require('compression');
-const logger = require("morgan")
+const express = require("express");
+const compression = require("compression");
+const logger = require("morgan");
 
+//*mongoose config
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
+const mongodb = mongoose.connection;
+mongodb.on("error", console.error.bind(console, "connection error:"));
+mongodb.once("open", () => console.log("Connected to database."));
+
+//*middleware
 const app = express();
-app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(compression());
-//* this exposes the whole public folder so that html routes are not necessary; eg app.get("/") not needed
 app.use(express.static("public"));
 
-app.use(require("./routes/apiRoutes.js"));
+app.use(logger("dev"));
+app.use(compression());
+
+app.use("/api", require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
+//*port config
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.error(`ERROR: ${err}`);
+  }
   console.log(`App running on port ${PORT}!`);
 });
